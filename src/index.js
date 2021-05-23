f1fecc9f431037022fc3d5fc47c16eade18c82c8
thailnv <56252864+thailnv@users.noreply.github.com>
2021-05-24 00:31:38 +0700

    change port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 //auto get index.js
 const route = require("./routes/index");
 
